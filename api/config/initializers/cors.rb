063323ef9f03208b5add467787e49d778647bccf
Sevenpros <sethbisoso@gmail.com>
2022-11-29 00:10:38 +0100

    Update README.md file

--- api/config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'localhost:3000' # <- Create React App port URL
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-  end
